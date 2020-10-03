0ffd94ef06af9577347e96cccde21025584e431a
eneroh <28052446+Eneroh@users.noreply.github.com>
2020-10-04 00:50:34 +1000

    Create dice.py

---
+++ dice.py
@@ -0,0 +1,15 @@
+"""
+Dice python program. Choice between 1 die vs 2 dice. STARTING OFF WITH PERSONAL CODING KNOWLEDGE
+"""
+
+import random
+
+choice = input("How many die?(1, 2): ")
+
+d1 = random.randint(0,6)
+d2 = random.randint(0,12)
+
+if choice == "1":
+	print("You rolled " + str(d1))
+if choice == "2":
+	print("You rolled " + str(d2))
