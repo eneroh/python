a7db34cb9963a6ba9c468007da95935fa7afa080
eneroh <28052446+Eneroh@users.noreply.github.com>
2020-10-04 00:01:57 +1000

    Create calculatorv3.py

---
+++ calculatorv3.py
@@ -0,0 +1,40 @@
+def add(x,y):
+	return x + y
+
+def subtract(x,y):
+	return x - y
+
+def multiply(x,y):
+	return x * y
+
+def divide(x,y):
+	return x / y
+
+while True:
+	print("Pick your operation.")
+	print("!. Add")
+	print("2. Subtract")
+	print("3. Multiply")
+	print("4. Divide")
+	op = input("Operation(1,2,3,4): ")
+
+	if op in ('1','2','3','4'):
+		x = float(input("first number: "))
+		y = float(input("second number: "))
+		
+		if op == "1":
+			print(x, "+", y, "=", add(x,y))
+
+		elif op == "2":
+			print(x, "-", y, "=", subtract(x,y))
+
+		elif op == "3":
+			print(x, "*", y, "=", multiply(x,y))
+		elif op == "4":
+			print(x, "/", y, "=", divide(x,y))
+		break
+	else:
+		print("Error, please try again")
+	print(exit)
+
+based on https://www.programiz.com/python-programming/examples/calculator
