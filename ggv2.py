9d80edb09daead4d8809e6b0e7268a21f71e9510
eneroh <28052446+Eneroh@users.noreply.github.com>
2020-10-04 00:31:39 +1000

    Create ggv2.py

---
+++ ggv2.py
@@ -0,0 +1,26 @@
+"""
+Guessing game.py
+
+Testing my python/general coding knowledge
+"""
+
+import random
+n = random.randint(0,9)
+win = False
+Turns = 0
+
+while win == False:
+	print("Guess the number")	
+	guess = int(input("Enter your guess(0-9): "))
+	Turns += 1
+	if guess == n:
+		print("Congratulations! Your guess " + str(guess) + " is correct")
+		print("Number of turns used: " + str(Turns))
+		win == True
+		break
+	else:
+		if guess < n:
+			print("Your guess " + str(guess) + " was too low, aim higher!")
+		else:
+			print(guess)
+			print("Your guess " + str(guess) + " was too high, aim lower!")
