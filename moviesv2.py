b1dd68412f1342fa429dd89792c136679b967dd0
eneroh <28052446+eneroh@users.noreply.github.com>
2022-06-20 02:00:33 +1000

    Update moviesv2.py

--- moviesv2.py
+++ moviesv2.py
@@ -6,7 +6,8 @@ moviesv2.py
 Me adding extra functionality to movies.py app    
     
 Specifically, input functionality to have a more inviting, intuitive user experience.    
-    
+
+How better to get people to care than to get them involved in the process? aha
 """    
     
 fMovie = input("Please enter your first favourite movie: ")    
