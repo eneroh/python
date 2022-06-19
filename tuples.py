b09f6d57f05c4565c3abd961722edc6a00e45718
eneroh <28052446+eneroh@users.noreply.github.com>
2022-06-20 01:36:45 +1000

    Create tuples.py

---
+++ tuples.py
@@ -0,0 +1,14 @@
+#!/bin/python                                                                                         
+    
+"""    
+tuples.py    
+    
+Immuteable, similar to static like in c programming, does not change    
+    
+Need to remember this for cybersec when reverse engineering -.-    
+"""    
+    
+grades = ("a", "b", "c", "d", "f")    
+    
+print(grades)    
+print(grades[1]) 
