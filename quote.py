f968c3ce2cd65d75c9f82a5d1ce903cea72430dc
eneroh <28052446+eneroh@users.noreply.github.com>
2022-06-19 23:50:51 +1000

    Create quote.py

---
+++ quote.py
@@ -0,0 +1,17 @@
+#!/bin/python                                                                   
+"""
+quote.py
+
+Consolidating my python knowledge
+
+Variables and methods/string manipulation    
+
+"""
+
+quote = "If you want peace, then prepare for war"    
+
+print(quote)    
+print(quote.upper())    
+print(quote.lower())    
+print(quote.title())    
+print(len(quote))   
