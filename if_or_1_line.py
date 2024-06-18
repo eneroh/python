6b6aca2d83730b14fd0a2e0285a3b37802097baa
eneroh <28052446+eneroh@users.noreply.github.com>
2024-06-18 20:45:19 +1000

    Create if_or_1_line.py

---
+++ if_or_1_line.py
@@ -0,0 +1,13 @@
+#!/bin/python
+# condenses if value to true/false eval, making a multiple line piece of code turn into one
+user_input = input('Name: ')
+
+#if user_input:
+#    name = user_input
+#
+#else:
+#    Name = 'N/A'
+#
+name = user_input or 'N/A'
+
+print(name)
